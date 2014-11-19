5bfde7974d2f0f1fe4ca6c0aa7d40b33609d9a97
Reid MacDonald <rmacdonald@rmacdonald-ltm.internal.salesforce.com>
2014-11-18 21:29:45 -0800

    adding brew/unbrew scripts

---
+++ bin/brew.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+set -A brewdirs Library share
+for brewdir in $brewdirs; do
+	dir="/usr/local/$brewdir" 
+  if ! [ -w $dir ]; then 
+		echo "Setting group writeable permssion for $dir"
+		chmod -R 775 $dir
+	fi
+done
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Check installation && update homebrew recipes
+brew doctor
+brew update
---
+++ bin/unbrew.sh
@@ -0,0 +1,1 @@
+rm -rf /usr/local/Cellar /usr/local/.git && brew cleanup
