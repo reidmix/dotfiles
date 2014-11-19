117080b6c686b2e5820034551d443cfe38c03f15
Reid MacDonald <reidmix@gmail.com>
2014-11-19 01:20:10 -0800

    installing all parts

---
+++ bin/all.sh
@@ -0,0 +1,20 @@
+# based on
+# https://github.com/MatthewMueller/dots
+# http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+
+bindir="$HOME/.files/bin"
+
+steps=(
+	brew
+	packages
+	setup-zsh
+)
+
+for step in "${steps[@]}"; do
+	echo "\033[1;32mRunning ${step}...\033[0m"
+	sh "$bindir/${step}.sh"
+done
+
+echo "\033[1;32mRunning link...\033[0m"
+zsh "$bindir/link.sh"
+
